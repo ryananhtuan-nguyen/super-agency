fa604905fc97b5c987d4fb953e929995f67fad0c
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-31 11:52:14 +1300

    feat: Landing page and navbar

---
+++ src/app/site/page.tsx
@@ -0,0 +1,103 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { pricingCards } from '@/lib/constants'
+import { cn } from '@/lib/utils'
+import { Check } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home() {
+  return (
+    <>
+      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col md:mb-20">
+        {/* Decoration */}
+        <div
+          className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"
+          aria-disabled
+        />
+        <div
+          className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"
+          aria-disabled
+        />
+
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
+            S#A
+          </h1>
+        </div>
+        <div className="flex justify-center items-center relative md:mt-[-70px]">
+          <Image
+            src={'/assets/preview.png'}
+            alt="banner image"
+            height={1200}
+            width={1200}
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+          />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
+        </div>
+      </section>
+      <section className="flex justify-center flex-col gap-4 sm:mt-20">
+        <h2 className="text-4xl text-center z-10 ">
+          Choose what fits you right
+        </h2>
+        <p className="text-muted-foreground text-center z-10">
+          Our pricing plans are tailored to meet your needs.
+          <br />
+          Get started or try it out for free.
+        </p>
+        <div className="flex justify-center gap-4 flex-wrap mt-6 pt-6">
+          {pricingCards.map((card) => (
+            <Card
+              key={card.title}
+              className={cn('w-[300px] flex flex-col justify-between', {
+                'border-2 border-primary': card.title === 'Unlimited Saas',
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={cn({
+                    'text-muted-foreground': card.title !== 'Unlimited Saas',
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>{card.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4">
+                <div>
+                  {card.features.map((feature) => (
+                    <div key={feature} className="flex gap-2 items-center">
+                      <Check className="text-muted-foreground" />
+                      <p>{feature}</p>
+                    </div>
+                  ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={cn(
+                    'w-full text-center bg-slate-700 p-2 rounded-md',
+                    {
+                      '!bg-muted-foreground': card.title === 'Unlimited Saas',
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
---
+++ src/components/site/navigation/index.tsx
@@ -0,0 +1,82 @@
+import { ModeToggle } from '@/components/global/mode-toggle'
+import { buttonVariants } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+import { User } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {
+  user?: null | User
+}
+
+const Navigation = ({ user }: Props) => {
+  return (
+    <div className="fixed top-0 right-0 left-0 p-4 flex items-center justify-between z-10">
+      <aside className="flex items-center gap-2">
+        <Image
+          src={'/assets/golden-logo.png'}
+          width={40}
+          height={40}
+          alt="logo"
+        />
+        <span className="text-xl font-bold">SupAgency.</span>
+      </aside>
+      <nav className="hidden md:block absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+        <ul className="flex items-center justify-center gap-8">
+          <Link
+            href={'#'}
+            className={buttonVariants({
+              variant: 'outline',
+              className: 'bg-transparent border-transparent text-slate-900',
+            })}
+          >
+            Pricing
+          </Link>
+          <Link
+            href={'#'}
+            className={buttonVariants({
+              variant: 'outline',
+              className: 'bg-transparent border-transparent text-slate-900',
+            })}
+          >
+            About
+          </Link>
+          <Link
+            href={'#'}
+            className={buttonVariants({
+              variant: 'outline',
+              className: 'bg-transparent border-transparent text-slate-900',
+            })}
+          >
+            Documentation
+          </Link>
+          <Link
+            href={'#'}
+            className={buttonVariants({
+              variant: 'outline',
+              className: 'bg-transparent border-transparent text-slate-900',
+            })}
+          >
+            Features
+          </Link>
+        </ul>
+      </nav>
+      <aside className="flex gap-2 items-center">
+        <Link
+          href={'/agency'}
+          className={buttonVariants({
+            variant: 'outline',
+            className: 'p-2 px-4',
+          })}
+        >
+          Login
+        </Link>
+        <UserButton />
+        <ModeToggle />
+      </aside>
+    </div>
+  )
+}
+
+export default Navigation
