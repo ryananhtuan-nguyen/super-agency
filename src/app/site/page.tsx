d86ccb888328144bd9d8cb05886f838a6eddad40
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-31 11:56:11 +1300

    style: change padding

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -42,7 +42,7 @@ export default async function Home() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
-      <section className="flex justify-center flex-col gap-4 sm:mt-20">
+      <section className="flex justify-center flex-col gap-4 md:!mt-20 mt-[-60px]">
         <h2 className="text-4xl text-center z-10 ">
           Choose what fits you right
         </h2>
