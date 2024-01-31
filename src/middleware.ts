9c81f59fe62223cbd219b7c356672e55fef5ba49
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-31 13:23:31 +1300

    feat: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,48 @@
 import { authMiddleware } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export default authMiddleware({
   publicRoutes: ['/site', '/api/uploadthing'],
+  async afterAuth(auth, req) {
+    //rewrite for domains
+    const url = req.nextUrl
+    const searchParams = url.searchParams.toString()
+    let hostname = req.headers
+
+    const pathWithSearchParams = `${url.pathname}${
+      searchParams.length > 0 ? `?${searchParams}` : ''
+    }`
+
+    //if subdomain exists
+    const customSubDomain = hostname
+      .get('host')
+      ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+      .filter(Boolean)[0]
+
+    if (customSubDomain) {
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+      )
+    }
+
+    if (url.pathname === '/sign-in' || url.pathname === '/sign-up') {
+      return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+    }
+
+    if (
+      url.pathname === '/' ||
+      (url.pathname === '/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    ) {
+      return NextResponse.rewrite(new URL('/site', req.url))
+    }
+
+    if (
+      url.pathname.startsWith('/agency') ||
+      url.pathname.startsWith('/subaccount')
+    ) {
+      return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
+    }
+  },
 })
 
 export const config = {
