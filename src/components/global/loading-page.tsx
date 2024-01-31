61be84597aff73c6e4cfe4822dceb58e655cd78a
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-01 11:53:51 +1300

    style: loading wrapper

---
+++ src/components/global/loading-page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loading from './loading'
+
+const LoadingPage = () => {
+  return (
+    <div className="h-full w-full flex justify-center items-center">
+      <Loading />
+    </div>
+  )
+}
+
+export default LoadingPage
