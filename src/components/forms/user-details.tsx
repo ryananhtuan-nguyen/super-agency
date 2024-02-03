a070b967fbb9f703d52c028ea8a4c0ec5870aadf
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-03 13:15:53 +1300

    feat: form handler

--- src/components/forms/user-details.tsx
+++ src/components/forms/user-details.tsx
@@ -1,7 +1,23 @@
 'use client'
-import { UserWithPermissionsAndSubAccounts } from '@/lib/types'
+import {
+  AuthUserWithAgencySigebarOptionsSubAccounts,
+  UserWithPermissionsAndSubAccounts,
+} from '@/lib/types'
+import { useModal } from '@/providers/modal-provider'
 import { SubAccount, User } from '@prisma/client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useToast } from '../ui/use-toast'
+import { useRouter } from 'next/navigation'
+import {
+  getAuthUserDetails,
+  getUserPermissions,
+  saveActivityLogsNotification,
+  updateUser,
+} from '@/lib/queries'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { UserDataSchema } from '@/lib/form-schemas/userDataSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 type Props = {
   id: string | null
@@ -13,6 +29,99 @@ type Props = {
 const UserDetails = ({ id, type, subAccounts, userData }: Props) => {
   const [subAccountPermissions, setSubAccountPermissions] =
     useState<UserWithPermissionsAndSubAccounts>(null)
+
+  const { data, setClose } = useModal()
+  const { toast } = useToast()
+  const router = useRouter()
+
+  const [authUserData, setAuthUserData] =
+    useState<AuthUserWithAgencySigebarOptionsSubAccounts>(null)
+  const [roleState, setRoleState] = useState('')
+  const [loadingPermission, setLoadingPermissions] = useState(false)
+
+  //get authuser
+
+  useEffect(() => {
+    if (data.user) {
+      const fetchDetails = async () => {
+        const response = await getAuthUserDetails()
+        if (response) setAuthUserData(response)
+      }
+      fetchDetails()
+    }
+  }, [])
+
+  const form = useForm<z.infer<typeof UserDataSchema>>({
+    resolver: zodResolver(UserDataSchema),
+    mode: 'onChange',
+    defaultValues: {
+      name: userData ? userData.name : data?.user?.name,
+      email: userData ? userData.email : data?.user?.email,
+      avatarUrl: userData ? userData.avatarUrl : data?.user?.avatarUrl,
+      role: userData ? userData.role : data?.user?.role,
+    },
+  })
+
+  //fetching permissions
+
+  useEffect(() => {
+    if (!data.user) return
+    const getPermissions = async () => {
+      if (!data.user) return
+      const permission = await getUserPermissions(data.user.id)
+      setSubAccountPermissions(permission)
+    }
+    getPermissions()
+  }, [data, form])
+
+  //reset form
+  useEffect(() => {
+    if (data.user) {
+      form.reset(data.user)
+    }
+
+    if (userData) {
+      form.reset(userData)
+    }
+  }, [data, userData])
+
+  //form handling
+  const onSubmit = async (values: z.infer<typeof UserDataSchema>) => {
+    if (!id) return
+    if (userData || data?.user) {
+      const updatedUser = await updateUser(values)
+
+      authUserData?.Agency?.SubAccount.filter((subacc) =>
+        authUserData.Permissions.find(
+          (p) => p.subAccountId === subacc.id && p.access
+        )
+      ).forEach(async (subaccount) => {
+        await saveActivityLogsNotification({
+          description: `Updated ${userData?.name} information`,
+          subaccountId: subaccount.id,
+        })
+      })
+
+      //successfully updated user
+      if (updatedUser) {
+        toast({
+          title: 'Success',
+          description: 'Updated user information',
+        })
+        setClose()
+        router.refresh()
+      } else {
+        toast({
+          title: 'Failed',
+          variant: 'destructive',
+          description: 'Could not update user information',
+        })
+      }
+    } else {
+      console.log('Error could not submit')
+    }
+  }
+
   return <div>UserDetails</div>
 }
 
