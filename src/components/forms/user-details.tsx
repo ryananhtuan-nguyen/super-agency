ad65d46cd2ce94db8e39f81035dc7dd8a1e05877
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-03 16:37:44 +1300

    feat: MVP user details form

--- src/components/forms/user-details.tsx
+++ src/components/forms/user-details.tsx
@@ -1,23 +1,55 @@
 'use client'
-import {
-  AuthUserWithAgencySigebarOptionsSubAccounts,
-  UserWithPermissionsAndSubAccounts,
-} from '@/lib/types'
-import { useModal } from '@/providers/modal-provider'
-import { SubAccount, User } from '@prisma/client'
-import React, { useEffect, useState } from 'react'
-import { useToast } from '../ui/use-toast'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { SubAccount, User } from '@prisma/client'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Input } from '@/components/ui/input'
+import { useToast } from '@/components/ui/use-toast'
+import FileUpload from '@/components/global/file-upload'
+import { useModal } from '@/providers/modal-provider'
+import { UserDataSchema } from '@/lib/form-schemas/userDataSchema'
 import {
+  changeUserPermission,
   getAuthUserDetails,
   getUserPermissions,
   saveActivityLogsNotification,
   updateUser,
 } from '@/lib/queries'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { UserDataSchema } from '@/lib/form-schemas/userDataSchema'
-import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import {
+  AuthUserWithAgencySigebarOptionsSubAccounts,
+  UserWithPermissionsAndSubAccounts,
+} from '@/lib/types'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+import { Button } from '../ui/button'
+import Loading from '../global/loading'
+import { Separator } from '../ui/separator'
+import { Switch } from '../ui/switch'
+import { v4 } from 'uuid'
 
 type Props = {
   id: string | null
@@ -37,7 +69,7 @@ const UserDetails = ({ id, type, subAccounts, userData }: Props) => {
   const [authUserData, setAuthUserData] =
     useState<AuthUserWithAgencySigebarOptionsSubAccounts>(null)
   const [roleState, setRoleState] = useState('')
-  const [loadingPermission, setLoadingPermissions] = useState(false)
+  const [loadingPermissions, setLoadingPermissions] = useState(false)
 
   //get authuser
 
@@ -61,6 +93,8 @@ const UserDetails = ({ id, type, subAccounts, userData }: Props) => {
       role: userData ? userData.role : data?.user?.role,
     },
   })
+
+  const isLoading = form.formState.isSubmitting
 
   //fetching permissions
 
@@ -122,7 +156,223 @@ const UserDetails = ({ id, type, subAccounts, userData }: Props) => {
     }
   }
 
-  return <div>UserDetails</div>
+  const onChangePermission = async (
+    subAccountId: string,
+    val: boolean,
+    permissionId: string | undefined
+  ) => {
+    if (!data.user?.email) return
+
+    setLoadingPermissions(true)
+
+    const response = await changeUserPermission(
+      permissionId ? permissionId : v4(),
+      data.user.email,
+      subAccountId,
+      val
+    )
+
+    if (type === 'agency') {
+      await saveActivityLogsNotification({
+        agencyId: authUserData?.Agency?.id,
+        description: `Gave ${userData?.name} access to | ${
+          subAccountPermissions?.Permissions.find(
+            (p) => p.subAccountId === subAccountId
+          )?.SubAccount.name
+        }`,
+
+        subaccountId: subAccountPermissions?.Permissions.find(
+          (p) => p.subAccountId === subAccountId
+        )?.SubAccount.id,
+      })
+    }
+
+    if (response) {
+      toast({
+        title: 'Success',
+        description: 'The request was successful',
+      })
+
+      if (subAccountPermissions) {
+        subAccountPermissions.Permissions.find((per) => {
+          if (per.subAccountId === subAccountId) {
+            return { ...per, access: !per.access }
+          }
+          return per
+        })
+      }
+    } else {
+      toast({
+        variant: 'destructive',
+        title: 'Failed',
+        description: 'Could not update permissions',
+      })
+    }
+
+    router.refresh()
+    setLoadingPermissions(false)
+  }
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>User Details</CardTitle>
+        <CardDescription>Add or update your information</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            {/* Form Fields */}
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="avatarUrl"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Profile picture</FormLabel>
+                  <FormControl>
+                    <FileUpload
+                      apiEndpoint="avatar"
+                      value={field.value}
+                      onChange={field.onChange}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>User full name</FormLabel>
+                  <FormControl>
+                    <Input required placeholder="Full Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      readOnly={userData?.role === 'AGENCY_OWNER' || isLoading}
+                      placeholder="Email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="role"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>User Role</FormLabel>
+                  <Select
+                    disabled={field.value === 'AGENCY_OWNER'}
+                    onValueChange={(value) => {
+                      if (
+                        value === 'SUBACCOUNT_USER' ||
+                        value === 'SUBACCOUNT_GUEST'
+                      ) {
+                        setRoleState(
+                          'You need to have subaccounts to assign Subaccount access to team member'
+                        )
+                      } else {
+                        setRoleState('')
+                      }
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select user role..." />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="AGENCY_ADMIN">Agency Admin</SelectItem>
+                      {(data?.user?.role === 'AGENCY_OWNER' ||
+                        userData?.role === 'AGENCY_OWNER') && (
+                        <SelectItem value="AGENCY_OWNER">
+                          Agency Owner
+                        </SelectItem>
+                      )}
+                      <SelectItem value="SUBACCOUNT_USER">
+                        Sub Account User
+                      </SelectItem>
+                      <SelectItem value="SUBACCOUNT_GUEST">
+                        Sub Account Guest
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <p className="text-muted-foreground">{roleState}</p>
+                </FormItem>
+              )}
+            />
+
+            {/* ACCESS CONTROL */}
+            <Button disabled={isLoading} type="submit">
+              {isLoading ? <Loading /> : 'Save User Details'}
+            </Button>
+
+            {/* Changing permissions section */}
+
+            {authUserData && authUserData.role == 'AGENCY_OWNER' && (
+              <div>
+                <Separator className="my-4" />
+                <FormLabel>User Permissions</FormLabel>
+                <FormDescription className="mb-4">
+                  This is only visible to agency owners
+                </FormDescription>
+                <div className="flex flex-col gap-4">
+                  {subAccounts?.map((subAccount) => {
+                    const subAccountPermissionsDetails =
+                      subAccountPermissions?.Permissions.find(
+                        (p) => p.subAccountId === subAccount.id
+                      )
+                    return (
+                      <div
+                        key={subAccount.id}
+                        className="flex items-center justify-between rounded-lg border p-4"
+                      >
+                        <div>
+                          <p>{subAccount.name}</p>
+                        </div>
+                        <Switch
+                          disabled={loadingPermissions}
+                          checked={subAccountPermissionsDetails?.access}
+                          onCheckedChange={(permission) => {
+                            onChangePermission(
+                              subAccount.id,
+                              permission,
+                              subAccountPermissionsDetails?.id
+                            )
+                          }}
+                        />
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            )}
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  )
 }
 
 export default UserDetails
