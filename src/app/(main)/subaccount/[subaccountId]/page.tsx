78b6baa068358bb3f41ce3cc7e229f92c5dd0070
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-06 16:14:15 +1300

    default page placeholder

---
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const SubAccountIdPage = (props: Props) => {
+  return <div>SubAccountIdPage</div>
+}
+
+export default SubAccountIdPage
---
+++ src/app/(main)/subaccount/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const SubAccountMainPage = (props: Props) => {
+  return <div>SubAccountMainPage</div>
+}
+
+export default SubAccountMainPage
