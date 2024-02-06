d9985e9a94976dc4afc570aabf1764eb1b1d13b8
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-06 17:28:23 +1300

    feat: sent invitation to other users

---
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -0,0 +1,65 @@
+import { db } from '@/lib/db'
+import React from 'react'
+import DataTable from './data-table'
+import { Plus } from 'lucide-react'
+import { currentUser } from '@clerk/nextjs'
+import { columns } from './columns'
+import SendInvitation from '@/components/forms/send-invitation'
+
+type Props = {
+  params: {
+    agencyId: string
+  }
+}
+
+const TeamPage = async ({ params }: Props) => {
+  const authUser = await currentUser()
+  if (!authUser) return null
+
+  const teamMembers = await db.user.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      },
+    },
+    include: {
+      Agency: {
+        include: {
+          SubAccount: true,
+        },
+      },
+      Permissions: {
+        include: {
+          SubAccount: true,
+        },
+      },
+    },
+  })
+
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    include: {
+      SubAccount: true,
+    },
+  })
+
+  if (!agencyDetails) return null
+
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+        </>
+      }
+      modalChildren={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue="name"
+      columns={columns}
+      data={teamMembers}
+    ></DataTable>
+  )
+}
+
+export default TeamPage
