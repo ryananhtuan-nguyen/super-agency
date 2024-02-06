fcc3ca4409127788f86d6efbaeae30e1a7a60e74
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-06 21:03:56 +1300

    added create pipeline form

--- src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelineId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelineId]/page.tsx
@@ -1,4 +1,10 @@
+import { Tabs, TabsList } from '@/components/ui/tabs'
+import { db } from '@/lib/db'
+import { getLanesWithTicketAndTags, getPipelineDetails } from '@/lib/queries'
+import { LaneDetail } from '@/lib/types'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import PipelineInfoBar from '../_components/pipeline-infobar'
 
 type Props = {
   params: {
@@ -10,7 +16,30 @@ type Props = {
 const PipelineIdPage = async ({ params }: Props) => {
   const pipelineDetails = await getPipelineDetails(params.pipelineId)
 
-  return <div>PipelineIdPage</div>
+  if (!pipelineDetails)
+    return redirect(`/subaccount/${params.subaccountId}/pipelines`)
+
+  const pipelines = await db.pipeline.findMany({
+    where: {
+      subAccountId: params.subaccountId,
+    },
+  })
+
+  const lanes = (await getLanesWithTicketAndTags(
+    params.pipelineId
+  )) as LaneDetail[]
+
+  return (
+    <Tabs defaultValue="view" className="w-full">
+      <TabsList className="bg-transparent border-b-2 h-16 w-full justify-between mb-4">
+        <PipelineInfoBar
+          pipelineId={params.pipelineId}
+          subaccountId={params.subaccountId}
+          pipelines={pipelines}
+        />
+      </TabsList>
+    </Tabs>
+  )
 }
 
 export default PipelineIdPage
