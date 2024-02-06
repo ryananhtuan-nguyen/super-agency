762150f7c60b1d2ce888c40777e5a9142f5bc583
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-06 19:21:31 +1300

    fix: added media card

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -1,6 +1,15 @@
-import { GetMediaFiles } from '@/lib/types'
 import React from 'react'
+
+import { GetMediaFiles } from '@/lib/types'
 import MediaUploadButton from './upload-button'
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '../ui/command'
+import MediaCard from './media-card'
 
 type Props = {
   data: GetMediaFiles
@@ -14,6 +23,22 @@ const MediaComponent = ({ data, subaccountId }: Props) => {
         <h1 className="text-4xl">Media Bucket</h1>
         <MediaUploadButton subaccountId={subaccountId} />
       </div>
+      <Command className="bg-transparent">
+        <CommandInput placeholder="Search for file name..." />
+        <CommandList className="pb-40 max-h-full">
+          <CommandEmpty>No media file.</CommandEmpty>
+          <div className="flex flex-wrap gap-4 pt-4">
+            {data?.Media.map((file) => (
+              <CommandItem
+                key={file.id}
+                className="p-0 max-w-[300px] w-full rounded-lg !bg-transparent !font-medium !text-white"
+              >
+                <MediaCard file={file} />
+              </CommandItem>
+            ))}
+          </div>
+        </CommandList>
+      </Command>
     </div>
   )
 }
