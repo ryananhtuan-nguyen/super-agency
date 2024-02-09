6903dac84ace8fe114c080782abeea87ca857efa
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-09 18:09:45 +1300

    edited stripe priceId

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -44,7 +44,7 @@ export const pricingCards = [
     duration: 'month',
     highlight: 'Key features',
     features: ['Rebilling', '24/7 Support team'],
-    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+    priceId: 'price_1OhmJYLS92xPfAtPHzu6odrF',
   },
   {
     title: 'Basic',
@@ -53,7 +53,7 @@ export const pricingCards = [
     duration: 'month',
     highlight: 'Everything in Starter, plus',
     features: ['Unlimited Sub accounts', 'Unlimited Team members'],
-    priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    priceId: 'price_1OhmJYLS92xPfAtPSsYnW8rb',
   },
 ]
 
--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -1,9 +1,8 @@
 import Stripe from 'stripe'
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
   apiVersion: '2023-10-16',
   appInfo: {
-    name: 'Super Agency App',
+    name: 'super-agency',
     version: '0.1.0',
   },
 })
