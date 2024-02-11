cf5495bc651ad367dfc497652d989abbd0356e52
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-02-11 15:46:51 +1300

    debugging

--- src/lib/stripe/stripe-client.ts
+++ src/lib/stripe/stripe-client.ts
@@ -1,16 +1,12 @@
 import { loadStripe, Stripe } from '@stripe/stripe-js'
 
 let stripePromise: Promise<Stripe | null>
-
 export const getStripe = (connectedAccountId?: string) => {
   if (!stripePromise) {
     stripePromise = loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!,
-      {
-        stripeAccount: connectedAccountId,
-      }
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? '',
+      { stripeAccount: connectedAccountId }
     )
   }
-
   return stripePromise
 }
